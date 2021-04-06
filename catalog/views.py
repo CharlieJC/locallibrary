8e3ddbde3b14060038cc9442361c0578749f2fa7
Charlie Cumming <charliejc0806@gmail.com>
2021-04-07 11:57:16 +1200

    Tutorial part 6-7

--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
 from django.views import generic
+
+
 # Create your views here.
 
 # home page
 
 
 def index(request):
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': Book.objects.all().count(),
         'num_instances': BookInstance.objects.all().count(),
         'num_instances_available': BookInstance.objects.filter(status__exact='a').count(),
-        'num_authors': Author.objects.count()
+        'num_authors': Author.objects.count(),
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
+
 
 class BookListView(generic.ListView):
     model = Book
@@ -25,6 +32,22 @@ class BookListView(generic.ListView):
     def get_queryset(self):
         return Book.objects.all()[:5]
 
+
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'books/detail.html'
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    context_object_name = 'author_list'
+    template_name = 'authors/authorlist.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        return Author.objects.all()[:5]
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    template_name = 'authors/detail.html'

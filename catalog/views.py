b636febd0cb552c559cdf0b1124a74a8ccb476ae
Charlie Cumming <charliejc0806@gmail.com>
2021-04-07 09:23:48 +1200

    Tutorial steps 5-6

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 # Create your views here.
 
 # home page
@@ -15,3 +16,15 @@ def index(request):
 
     return render(request, 'index.html', context=context)
 
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/booklist.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        return Book.objects.all()[:5]
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'books/detail.html'
